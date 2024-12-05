b00d2ef47efcb55eb6b568115048f2d4717dc7db
Hojae20 <71290043+Hojae20@users.noreply.github.com>
2024-12-05 13:12:46 +0100

    Update stream1.sh

--- Raspberry/stream1.sh
+++ Raspberry/stream1.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-INPUT_DIR="./datasets/datasets_20hz_1_robot_1_minute"
-OUTPUT_DIR="./datasets/merged_datasets/merged_dataset.csv"
+INPUT_DIR="./datasets/right_arm.csv"
+OUTPUT_DIR="./datasets/datasets_20hz_1_robot_1_minute"
 
 echo "Input folder: $INPUT_DIR"
 echo "Output folder: $OUTPUT_DIR"
 
-java -jar merge_data-1.0-SNAPSHOT.jar "$INPUT_DIR" "$OUTPUT_DIR"
+java -jar javaproject-1.jar "$INPUT_DIR" "$OUTPUT_DIR"
