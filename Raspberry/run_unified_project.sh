bd733a33d9162800006bed6aae976a5991732b3e
Gorm2303 <gormdk333@gmail.com>
2024-12-16 12:53:40 +0100

    Create run_unified_project.sh

---
+++ Raspberry/run_unified_project.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+INPUT_DIR="./datasets/datasets_20hz_1_robot_1_minute"
+OUTPUT_DIR="./datasets/merged_datasets/merged_dataset.csv"
+JAR_FILE="unified_project-1.0-SNAPSHOT_fixed.jar"
+
+echo "Input folder: $INPUT_DIR"
+echo "Output folder: $OUTPUT_DIR"
+echo "JAR file: $JAR_FILE"
+
+java -Xms1g -Xmx2g -jar "$JAR_FILE" "$INPUT_DIR" "$OUTPUT_DIR"
