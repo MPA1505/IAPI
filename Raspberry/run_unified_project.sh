2b25ab3923287edebfddd07b5fe9492b9e737d71
Gorm2303 <gormdk333@gmail.com>
2024-12-16 12:57:30 +0100

    Update run_unified_project.sh

--- Raspberry/run_unified_project.sh
+++ Raspberry/run_unified_project.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 INPUT_DIR="./datasets/datasets_20hz_1_robot_1_minute"
-OUTPUT_DIR="./datasets/merged_datasets/merged_dataset.csv"
+OUTPUT_DIR="./datasets/merged_datasets/merged_dataset.parquet"
 JAR_FILE="unified_project-1.0-SNAPSHOT_fixed.jar"
 
 echo "Input folder: $INPUT_DIR"
